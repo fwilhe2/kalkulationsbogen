1edebd32828f8e0e51603ef60ebcbaf16aae23be
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-10-11 22:08:30 +0200

    wip cell styles

--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -6,6 +6,7 @@ export type formulaCell = cellWithFunction & cellWithRange;
 type cellWithValue = {
   value: string; // | number
   valueType?: valueType;
+  isInputCell?: boolean;
 };
 type cellWithFunction = {
   functionName: string;
@@ -85,7 +86,11 @@ function tableCellElement(cell: cell): string {
   }
 
   if ("functionName" in cell) {
-    return `<table:table-cell table:formula="of:=${cell.functionName}(${Array.isArray(cell.arguments) ? cell.arguments.join(";") : cell.arguments})" />`;
+    return `<table:table-cell table:formula="of:=${cell.functionName}(${Array.isArray(cell.arguments) ? cell.arguments.join(";") : cell.arguments})" table:style-name="CALCULATED_STYLE" />`;
+  }
+
+  if (cell.isInputCell) {
+    return `<table:table-cell office:value="${cell.value}" table:style-name="INPUT_STYLE" office:value-type="${cell.valueType}" calcext:value-type="${cell.valueType}" />`;
   }
 
   if (cell.valueType === "float") {
@@ -191,6 +196,13 @@ const FODS_TEMPLATE = `<?xml version="1.0" encoding="UTF-8"?>
             <number:text>%</number:text>
         </number:percentage-style>
         <style:style style:name="PERCENTAGE_STYLE" style:family="table-cell" style:parent-style-name="Default" style:data-style-name="__PERCENTAGE_STYLE" />
+        <style:style style:name="CALCULATED_STYLE" style:family="table-cell">
+          <style:table-cell-properties fo:background-color="#f2f2f2" fo:border="0.06pt solid #3f3f3f"/>
+        </style:style>
+        <style:style style:name="INPUT_STYLE" style:family="table-cell" style:parent-style-name="Default">
+          <style:table-cell-properties fo:background-color="#ffcc99" fo:border="0.06pt solid #7f7f7f"/>
+          <style:text-properties fo:color="#3f3f76"/>
+        </style:style>
     </office:automatic-styles>
     <office:body>
         <office:spreadsheet>
